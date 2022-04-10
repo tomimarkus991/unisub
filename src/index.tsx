bb224a66fc347c1e18264b307b6ceaac04ea9e47
tomimarkus991 <tomimarkusalber@gmail.com>
2022-04-10 11:34:26 +0300

    Update react

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import "./index.css";
 
 import { LocalizationProvider } from "@mui/lab";
 import AdapterMoment from "@mui/lab/AdapterMoment";
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import { SidebarProvider, SubProvider } from "context";
@@ -12,32 +11,18 @@ import { Router } from "routes";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-// import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root") as Element);
 
-// @ts-ignore
-// const root = createRoot(document.getElementById("root"));
-
-// root.render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
-// npm i react@latest react-dom@latest
-// npm i -D @types/react@latest @types/react-dom@latest
-
-ReactDOM.render(
+root.render(
   <BrowserRouter>
-    <StrictMode>
-      <LocalizationProvider dateAdapter={AdapterMoment}>
-        <SidebarProvider>
-          <SubProvider>
-            <Router />
-          </SubProvider>
-        </SidebarProvider>
-      </LocalizationProvider>
-    </StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <SidebarProvider>
+        <SubProvider>
+          <Router />
+        </SubProvider>
+      </SidebarProvider>
+    </LocalizationProvider>
+  </BrowserRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
