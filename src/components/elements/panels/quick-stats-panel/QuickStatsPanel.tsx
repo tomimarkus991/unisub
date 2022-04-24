118b20b86a65448c8e9864afd00312ddbdc5c218
tomimarkus991 <tomimarkusalber@gmail.com>
2022-04-24 11:29:47 +0300

    Improve responsiveness

--- src/components/elements/panels/quick-stats-panel/QuickStatsPanel.tsx
+++ src/components/elements/panels/quick-stats-panel/QuickStatsPanel.tsx
@@ -35,28 +35,30 @@ export const QuickStatsPanel = () => {
   }, [subs, subPanelBillingType]);
 
   return (
-    <div className="flex flex-row px-6 w-[95%] h-36 bg-white rounded-xl shadow-lg cursor-default select-none min:w-[20rem]">
-      <div className="flex flex-col justify-center items-center mr-2">
+    <div className="flex flex-row px-4 w-[95%] h-36 bg-white rounded-xl shadow-lg cursor-default select-none min:px-8 min:w-[20rem]">
+      <div className="flex flex-col justify-center items-center mr-2 min-w-[5rem] min:mr-0 xs:mr-4">
         <div className="max-w-[5rem]">
-          <img className="w-24 h-24" alt="user" src={`/stats/stats.svg`} />
+          <img className="w-16 h-16 min:w-24 min:h-24" alt="user" src={`/stats/stats.svg`} />
         </div>
       </div>
       <div className="flex flex-col flex-1 justify-center items-center">
         <div
           className={
             (clsx(scrollbarStyles),
-            "flex flex-row mb-2 overflow-x-auto max-w-[11rem] overflow-y-hidden text-ellipsis whitespace-nowrap")
+            "flex flex-row mb-2 text-2xl overflow-x-auto max-w-[11rem] overflow-y-hidden text-ellipsis whitespace-nowrap")
           }
         >
           <CountUp start={totalValue - 3} end={totalValue} duration={0.3} decimals={decimals}>
-            {({ countUpRef }) => <p ref={countUpRef as any} className="text-4xl font-bold" />}
+            {({ countUpRef }) => (
+              <p ref={countUpRef as any} className="text-3xl font-bold min:text-4xl" />
+            )}
           </CountUp>
-          <p className="text-4xl font-semibold cursor-pointer">€</p>
+          <p className="text-3xl font-semibold cursor-pointer min:text-4xl">€</p>
         </div>
         <div className="flex flex-row justify-center items-center">
-          <p className="mr-2 text-2xl font-medium">{subPanelBillingType}</p>
+          <p className="mr-2 text-lg font-medium min:text-2xl">{subPanelBillingType}</p>
           <div
-            className="text-2xl text-gray-800 cursor-pointer"
+            className="text-lg text-gray-800 cursor-pointer min:text-2xl"
             onClick={() =>
               setSubPanelBillingType(currentType => {
                 const index = billingTypeValues.indexOf(currentType) + 1;
