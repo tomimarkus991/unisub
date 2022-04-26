cfca6eb031668305da1ffa35f1d795348e3f99ef
tomimarkus991 <tomimarkusalber@gmail.com>
2022-04-26 16:32:03 +0300

    Remove animations

--- src/components/elements/modals/modal/Modal.tsx
+++ src/components/elements/modals/modal/Modal.tsx
@@ -1,6 +1,5 @@
 import { Dialog } from "@headlessui/react";
 import clsx from "clsx";
-import { AnimatePresence, motion } from "framer-motion";
 import { ReactNode, useRef } from "react";
 
 const modalMaxWidth = {
@@ -32,42 +31,41 @@ export const Modal = ({ children, modalButton, open, setOpen, maxWidth = "xl" }:
         open={open}
         onClose={setOpen}
       >
-        <AnimatePresence>
-          <motion.div
-            id="overlay"
-            ref={initialFocusRef}
-            key="app-modal-overlay"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 0.5 }}
-            transition={{ duration: 0.1, ease: "easeIn" }}
-            exit={{ opacity: 0 }}
-            onClick={() => setOpen(false)}
-            className="absolute inset-0 w-full h-full bg-gray-500"
-          />
-          <motion.div
-            id="modal-children"
-            key="app-modal-children"
-            initial={{ opacity: 0.8, scale: 0.95 }}
-            animate={{
-              opacity: 1,
-              scale: 1,
-              transition: {
-                duration: 0.2,
-                ease: "easeIn",
-              },
-            }}
-            exit={{
-              opacity: 0,
-              scale: 0.95,
-            }}
-            className={clsx(
-              "z-[70] min-w-[95%] max-w-[94%] bg-white rounded-xl min:min-w-[20rem]",
-              modalMaxWidth[maxWidth]
-            )}
-          >
-            {children}
-          </motion.div>
-        </AnimatePresence>
+        <div
+          id="overlay"
+          ref={initialFocusRef}
+          key="app-modal-overlay"
+          role="button"
+          tabIndex={0}
+          // initial={{ opacity: 0.4 }}
+          // animate={{ opacity: 0.5 }}
+          // transition={{ duration: 0.4, ease: "easeIn" }}
+          // exit={{ opacity: 0 }}
+          onClick={() => setOpen(false)}
+          className="absolute inset-0 w-full h-full bg-gray-500 opacity-40"
+        />
+        <div
+          id="modal-children"
+          key="app-modal-children"
+          // initial={{ scale: 0.95 }}
+          // animate={{
+          //   scale: 1,
+          //   transition: {
+          //     duration: 0.2,
+          //     ease: "easeIn",
+          //   },
+          // }}
+          // exit={{
+          //   opacity: 0,
+          //   scale: 0.95,
+          // }}
+          className={clsx(
+            "z-[70] min-w-[95%] max-w-[94%] bg-white rounded-xl min:min-w-[20rem]",
+            modalMaxWidth[maxWidth]
+          )}
+        >
+          {children}
+        </div>
       </Dialog>
     </>
   );
