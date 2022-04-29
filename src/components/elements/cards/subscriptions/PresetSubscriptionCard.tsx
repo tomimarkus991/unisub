f4600783617c345fb5472420d8283ddf64ff24ee
tomimarkus991 <tomimarkusalber@gmail.com>
2022-04-29 20:15:15 +0300

    Improve preset card

--- src/components/elements/cards/subscriptions/PresetSubscriptionCard.tsx
+++ src/components/elements/cards/subscriptions/PresetSubscriptionCard.tsx
@@ -16,7 +16,7 @@ export const PresetSubscriptionCard = ({ subValues }: Props) => {
   return (
     <div
       className={clsx(
-        "flex overflow-hidden relative flex-row items-center px-8 w-full h-20 rounded-2xl ring-1 ring-black ring-opacity-5",
+        "flex overflow-hidden relative flex-row items-center px-4 h-20 rounded-2xl ring-1 ring-black ring-opacity-5 sm:px-2",
         cardColors[color]
       )}
     >
@@ -24,7 +24,7 @@ export const PresetSubscriptionCard = ({ subValues }: Props) => {
       <div className="flex z-10 flex-col ml-2 w-9/12 sm:ml-6">
         <p
           className={clsx(
-            "overflow-x-auto mb-2 whitespace-nowrap sm:mb-1 scrollbar-styles",
+            "overflow-x-auto mb-1 w-[10rem] whitespace-nowrap sm:w-[15rem] scrollbar-styles",
             "text-xl font-semibold xs:text-2xl",
             textColor
           )}
