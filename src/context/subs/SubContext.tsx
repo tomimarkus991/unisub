8adc656977d5d01317a30127c5419fa32cbd76e1
tomimarkus991 <tomimarkusalber@gmail.com>
2022-05-07 15:31:19 +0300

    Add more subs

--- src/context/subs/SubContext.tsx
+++ src/context/subs/SubContext.tsx
@@ -1,7 +1,7 @@
 import moment from "moment";
 import React, { createContext, Dispatch, SetStateAction, useContext, useState } from "react";
 
-import { mapSubTypeToMomentType } from "app-constants";
+import { cardColors, mapSubTypeToMomentType } from "app-constants";
 import { Subscription } from "types";
 
 import { generateAllCosts } from "../../utils";
@@ -52,12 +52,14 @@ const SubContext = createContext(initContextData);
 export const useSub = () => useContext(SubContext);
 
 export const SubProvider = ({ children }: ProviderProps) => {
-  const [subs, setSubs] = useState<Subscription[]>([
-    {
-      id: "1",
-      title: "Disney+",
+  // const subs = cardColors.
+  // make an array of cardColors
+  const createSubs: Subscription[] = Object.keys(cardColors).map((color: any) => {
+    return {
+      id: `${color}`,
+      title: `${color}`,
       category: "Streaming",
-      color: "darkBlue",
+      color,
       active: true,
       billingType: "monthly",
       currency: "EUR",
@@ -65,20 +67,37 @@ export const SubProvider = ({ children }: ProviderProps) => {
       startDate: moment(new Date()).unix(),
       nextPaymentDate: moment(new Date()).add(1, mapSubTypeToMomentType("monthly")).unix(),
       cost: 10,
-    },
-    {
-      id: "2",
-      title: "Netflix",
-      category: "Streaming",
-      color: "red",
-      active: true,
-      billingType: "monthly",
-      currency: "EUR",
-      allCosts: generateAllCosts(15, "monthly"),
-      startDate: moment(new Date()).unix(),
-      nextPaymentDate: moment(new Date()).add(1, mapSubTypeToMomentType("monthly")).unix(),
-      cost: 15,
-    },
+    };
+  });
+
+  const [subs, setSubs] = useState<Subscription[]>([
+    ...createSubs,
+    // {
+    //   id: "1",
+    //   title: "Disney+",
+    //   category: "Streaming",
+    //   color: "darkBlue",
+    //   active: true,
+    //   billingType: "monthly",
+    //   currency: "EUR",
+    //   allCosts: generateAllCosts(10, "monthly"),
+    //   startDate: moment(new Date()).unix(),
+    //   nextPaymentDate: moment(new Date()).add(1, mapSubTypeToMomentType("monthly")).unix(),
+    //   cost: 10,
+    // },
+    // {
+    //   id: "2",
+    //   title: "Netflix",
+    //   category: "Streaming",
+    //   color: "red",
+    //   active: true,
+    //   billingType: "monthly",
+    //   currency: "EUR",
+    //   allCosts: generateAllCosts(15, "monthly"),
+    //   startDate: moment(new Date()).unix(),
+    //   nextPaymentDate: moment(new Date()).add(1, mapSubTypeToMomentType("monthly")).unix(),
+    //   cost: 15,
+    // },
     {
       id: "3",
       title: "Spotify",
