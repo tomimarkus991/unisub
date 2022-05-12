2b6fcc942bfb3e042bf2efce22afceb75cf600cc
tomimarkus991 <tomimarkusalber@gmail.com>
2022-05-12 22:48:00 +0300

    Remove darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/*.{js,jsx,ts,tsx}"],
-  darkMode: "class",
+  // darkMode: "class",
   theme: {
     extend: {
       screens: {
